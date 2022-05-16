67162a14814026dcbaf5005deb65028e90a863e8
kodeb8 <maxtitorzimmerman@gmail.com>
2022-05-16 15:05:48 -0400

    Set up webpack environment

---
+++ dist/bundle88dea3fc4926ac1f5eee.js
@@ -0,0 +1,9 @@
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+console.log("hi");
+/******/ })()
+;
+//# sourceMappingURL=bundle88dea3fc4926ac1f5eee.js.map
